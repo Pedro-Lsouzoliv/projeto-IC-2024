b46d7b087a579047cea43e959fe43d0743e8f5c4
Pedro Luis de Souza Oliveira <149951447+Pedro-Lsouzoliv@users.noreply.github.com>
2026-09-06 20:39:00 -0300

    Refactor code to remove first column from files

--- extrair dados/remover_coluna_time.m
+++ extrair dados/remover_coluna_time.m
@@ -1,12 +1,11 @@
 %mesma coisa do remover coluna, mas criando arquivos novos
-%rapaz, esse aqui ta top
 path = 'C:\Users\pokel\Desktop\Pedro\UFBA- Bi cit\Ic UFBA\dados cspam sets\asccount';%pasta de destino dos arquivos novos
 lista = dir(fullfile(path, '*.asc'));
 
 for i = 1:length(lista)
     arquivo_atual = fullfile(path, lista(i).name);
     
-    % Lê como tabela
+    % LÃª como tabela
     T = readtable(arquivo_atual, 'FileType', 'text', 'Delimiter', '\t');
 
     % Remove a primeira coluna
